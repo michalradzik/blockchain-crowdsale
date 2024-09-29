3c73405532d9bb697cb35d48e25b6de9f880e826
Micrad <leszekwita@o2.pl>
2024-09-29 17:50:53 +0200

    added Sepholia network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,32 @@
 require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { PRIVATE_KEYS, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } = process.env;
+
 module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.5.5",  // Dla kontraktów OpenZeppelin Crowdsale
+        version: "0.5.5", 
       },
       {
-        version: "0.8.9",  // Dla nowych kontraktów w wersji 0.8.x
+        version: "0.8.9",
       },
     ],
+  },
+  networks: {
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: PRIVATE_KEYS ? PRIVATE_KEYS.split(",") : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
   },
 };
 
